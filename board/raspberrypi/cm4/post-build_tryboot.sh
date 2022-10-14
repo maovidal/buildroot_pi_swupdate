542e5d96b5759d6ca0f387043cfe3695679d8e9e
Mauricio Vidal <maovidal@users.noreply.github.com>
2022-10-14 11:59:36 -0500

    Minor `CM4` `tryboot` corrections - No working yet

--- board/raspberrypi/cm4/post-build_tryboot.sh
+++ board/raspberrypi/cm4/post-build_tryboot.sh
@@ -10,6 +10,9 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
 
+# An indicator for the empty partition
+install -D -m 0644 ${BR2_EXTERNAL_PISWU_CFG_PATH}/board/raspberrypi/common/empty ${BINARIES_DIR}/persistent/.dummy
+
 # Every boot image receives a custom cmdline.txt, config.txt and tryboot.txt
 install -D -m 0644 ${BR2_EXTERNAL_PISWU_CFG_PATH}/board/raspberrypi/cm4/cmdline_a_tryboot.txt ${BINARIES_DIR}/rpi-firmware_a/cmdline.txt
 install -D -m 0644 ${BR2_EXTERNAL_PISWU_CFG_PATH}/board/raspberrypi/cm4/cmdline_b_tryboot.txt ${BINARIES_DIR}/rpi-firmware_b/cmdline.txt
@@ -19,9 +22,6 @@ install -D -m 0644 ${BR2_EXTERNAL_PISWU_CFG_PATH}/board/raspberrypi/cm4/config_b
 
 install -D -m 0644 ${BR2_EXTERNAL_PISWU_CFG_PATH}/board/raspberrypi/cm4/tryboot_a.txt ${BINARIES_DIR}/rpi-firmware_a/tryboot.txt
 install -D -m 0644 ${BR2_EXTERNAL_PISWU_CFG_PATH}/board/raspberrypi/cm4/tryboot_b.txt ${BINARIES_DIR}/rpi-firmware_b/tryboot.txt
-
-# An indicator for the empty partition
-install -D -m 0644 ${BR2_EXTERNAL_PISWU_CFG_PATH}/board/raspberrypi/common/empty ${BINARIES_DIR}/persistent/.dummy
 
 
 # Mount partitions
