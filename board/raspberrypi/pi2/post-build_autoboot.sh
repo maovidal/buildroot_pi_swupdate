55208b8a2355ebc1440802dfa2ba9abd582b3ac3
Mauricio Vidal <maovidal@users.noreply.github.com>
2022-10-14 12:01:33 -0500

    Minor `Pi2` `autoboot` no booting yet

--- board/raspberrypi/pi2/post-build_autoboot.sh
+++ board/raspberrypi/pi2/post-build_autoboot.sh
@@ -23,19 +23,16 @@ install -D -m 0644 ${BR2_EXTERNAL_PISWU_CFG_PATH}/board/raspberrypi/pi2/config.t
 install -D -m 0644 ${BR2_EXTERNAL_PISWU_CFG_PATH}/board/raspberrypi/pi2/config.txt ${BINARIES_DIR}/rpi-firmware_a/tryboot.txt
 install -D -m 0644 ${BR2_EXTERNAL_PISWU_CFG_PATH}/board/raspberrypi/pi2/config.txt ${BINARIES_DIR}/rpi-firmware_b/tryboot.txt
 
-# An indicator for the empty partition
-install -D -m 0644 ${BR2_EXTERNAL_PISWU_CFG_PATH}/board/raspberrypi/common/empty ${BINARIES_DIR}/persistent/.dummy
-
 
 # Mount partitions
 if [ -e ${TARGET_DIR}/etc/fstab ]; then
+	# Persistent (Where autoboot.txt resides)
+	grep -qE '/dev/mmcblk0p1' ${TARGET_DIR}/etc/fstab || \
+	echo "/dev/mmcblk0p1 /persistent vfat defaults,noatime 0 0" >> ${TARGET_DIR}/etc/fstab
 	# Boot_A
-	grep -qE '/dev/mmcblk0p5' ${TARGET_DIR}/etc/fstab || \
-	echo "/dev/mmcblk0p5 /boot_a vfat defaults,noatime 0 0" >> ${TARGET_DIR}/etc/fstab
-	# Boot_B
-	grep -qE '/dev/mmcblk0p7' ${TARGET_DIR}/etc/fstab || \
-	echo "/dev/mmcblk0p7 /boot_b vfat defaults,noatime 0 0" >> ${TARGET_DIR}/etc/fstab
-	# Persistent
 	grep -qE '/dev/mmcblk0p2' ${TARGET_DIR}/etc/fstab || \
-	echo "/dev/mmcblk0p2 /persistent vfat defaults,noatime 0 0" >> ${TARGET_DIR}/etc/fstab
+	echo "/dev/mmcblk0p2 /boot_a vfat defaults,noatime 0 0" >> ${TARGET_DIR}/etc/fstab
+	# Boot_B
+	grep -qE '/dev/mmcblk0p3' ${TARGET_DIR}/etc/fstab || \
+	echo "/dev/mmcblk0p3 /boot_b vfat defaults,noatime 0 0" >> ${TARGET_DIR}/etc/fstab
 fi
