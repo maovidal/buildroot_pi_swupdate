26cb35bbe1a6602a71734ca53c57f794a40ce16a
Mauricio Vidal <maovidal@users.noreply.github.com>
2022-10-16 12:56:00 -0500

    `autoboot` on `CM4` for testing

--- board/raspberrypi/cm4/post-build_autoboot.sh
+++ board/raspberrypi/cm4/post-build_autoboot.sh
@@ -14,8 +14,8 @@ fi
 install -D -m 0644 ${BR2_EXTERNAL_PISWU_CFG_PATH}/board/raspberrypi/common/autoboot.txt ${BINARIES_DIR}/autoboot.txt
 
 # Every boot image receives a custom cmdline.txt, config.txt and tryboot.txt
-install -D -m 0644 ${BR2_EXTERNAL_PISWU_CFG_PATH}/board/raspberrypi/cm4/cmdline_a_autoboot.txt ${BINARIES_DIR}/rpi-firmware_a/cmdline.txt
-install -D -m 0644 ${BR2_EXTERNAL_PISWU_CFG_PATH}/board/raspberrypi/cm4/cmdline_b_autoboot.txt ${BINARIES_DIR}/rpi-firmware_b/cmdline.txt
+install -D -m 0644 ${BR2_EXTERNAL_PISWU_CFG_PATH}/board/raspberrypi/cm4/cmdline_a.txt ${BINARIES_DIR}/rpi-firmware_a/cmdline.txt
+install -D -m 0644 ${BR2_EXTERNAL_PISWU_CFG_PATH}/board/raspberrypi/cm4/cmdline_b.txt ${BINARIES_DIR}/rpi-firmware_b/cmdline.txt
 
 install -D -m 0644 ${BR2_EXTERNAL_PISWU_CFG_PATH}/board/raspberrypi/cm4/config_a.txt ${BINARIES_DIR}/rpi-firmware_a/config.txt
 install -D -m 0644 ${BR2_EXTERNAL_PISWU_CFG_PATH}/board/raspberrypi/cm4/config_b.txt ${BINARIES_DIR}/rpi-firmware_b/config.txt
