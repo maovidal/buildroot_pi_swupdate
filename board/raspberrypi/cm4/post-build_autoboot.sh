8f6362bccf5b8ade9f07aba1edbedca8a181fbcf
Mauricio Vidal <maovidal@users.noreply.github.com>
2022-10-16 15:53:25 -0500

    `autoboot` on `CM4` working

--- board/raspberrypi/cm4/post-build_autoboot.sh
+++ board/raspberrypi/cm4/post-build_autoboot.sh
@@ -13,6 +13,9 @@ fi
 # Merveilleux autoboot
 install -D -m 0644 ${BR2_EXTERNAL_PISWU_CFG_PATH}/board/raspberrypi/common/autoboot.txt ${BINARIES_DIR}/autoboot.txt
 
+# Base config.txt
+install -D -m 0644 ${BR2_EXTERNAL_PISWU_CFG_PATH}/board/raspberrypi/cm4/config_base.txt ${BINARIES_DIR}/config_base.txt
+
 # Every boot image receives a custom cmdline.txt, config.txt and tryboot.txt
 install -D -m 0644 ${BR2_EXTERNAL_PISWU_CFG_PATH}/board/raspberrypi/cm4/cmdline_a.txt ${BINARIES_DIR}/rpi-firmware_a/cmdline.txt
 install -D -m 0644 ${BR2_EXTERNAL_PISWU_CFG_PATH}/board/raspberrypi/cm4/cmdline_b.txt ${BINARIES_DIR}/rpi-firmware_b/cmdline.txt
