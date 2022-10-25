1e07d31e6d9a43f826d39995f29107102cb7379a
Mauricio Vidal <maovidal@users.noreply.github.com>
2022-10-24 22:33:49 -0500

    Corrected script post update

--- board/raspberrypi/generic/overlay_swupdate/etc/swupdate/post_update.sh
+++ board/raspberrypi/generic/overlay_swupdate/etc/swupdate/post_update.sh
@@ -3,34 +3,12 @@
 # www.vidalastudillo.com
 
 #
-# Performs the following actions once SWUpdate has written the partitions
-# with updated content:
-#
-# 1. Reboots the device asking to use the set of paritions just updated.
+# Performs actions once SWUpdate has written the partitions
 #
 
-
-# Gets the current partition
-sh /etc/swupdate/current_partition.sh
-current_partition=$?
 
 # Forces an immediate disk update
 sync
 
 # Reboots asking to use the updated set of partitions
-if [ "$current_partition" -eq 100 ] ; then
-
-    rebootp 3
-
-elif [ "$current_partition" -eq 200 ] ; then
-
-    rebootp 2
-
-else
-
-    # Decides to halt the system
-    echo "ERROR: The current partition is not the expected."
-    echo "The device will halt to prevent further damage."
-    halt
-
-fi
+rebootp '0 tryboot'
