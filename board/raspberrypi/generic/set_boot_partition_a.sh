17df15a0805692ecea0f7860e6cd805652e86eef
Mauricio Vidal <maovidal@users.noreply.github.com>
2022-10-24 20:05:00 -0500

    `Pi2` with `tryboot_a_b`

--- board/raspberrypi/generic/set_boot_partition_a.sh
+++ board/raspberrypi/generic/set_boot_partition_a.sh
@@ -6,5 +6,10 @@
 # Updates the autoboot.txt to tell a RaspberryPi to boot from the partition A
 # 
 
-echo "boot_partition=2" > '/persistent/autoboot.txt'
+echo "[all]
+tryboot_a_b=1
+boot_partition=2
+[tryboot]
+boot_partition=3
+" > '/persistent/autoboot.txt'
 echo "Next reboot will load from: boot_a"
